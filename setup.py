b96135c57a54a11a21e7013d7f351ceb9f628fed
Om <omshandilya.work@gmail.com>
2025-03-01 23:32:10 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='generative_ai_project',  
+    version='0.0.1',  
+    author='Om Shandilya',
+    author_email='omshandilya.work@gmail.com',
+    packages=find_packages(),  
+    install_requires=[]  
+)
+
